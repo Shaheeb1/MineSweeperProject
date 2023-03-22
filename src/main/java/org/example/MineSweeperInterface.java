b14b579e9c3a1e418453d4c89b7a0d3dcec6d519
Shaheeb <shaheebsabir@gmail.com>
2023-03-22 17:23:21 +0000

    Fixed play method

--- src/main/java/org/example/MineSweeperInterface.java
+++ src/main/java/org/example/MineSweeperInterface.java
@@ -1,8 +1,20 @@
 package org.example;
 
 public interface MineSweeperInterface {
+    static final int ROWS = 10;
+    static final int COLS = 10;
+    static final int MINES = 10;
+
+    boolean[][] board = new boolean[ROWS][COLS];
+    boolean[][] revealed = new boolean[ROWS][COLS];
 
     void intializeBoard();
 
     void printBoard();
+
+    public default boolean getboard() {
+        return board[ROWS][COLS];
+    }
+
+
 }
